e26ef5efa964f1330ae70985ca8d83846fd38713
opilane <opilane@HP-CZC0359NN7>
2024-04-24 13:40:05 +0300

    m

---
+++ visual novel/Music.cs
@@ -0,0 +1,29 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace visual_novel
+{
+    class Music
+    {
+        WMPLib.WindowsMediaPlayer Player;
+        public void PlayFile(String url)
+        {
+            WMPLib.WindowsMediaPlayer Player = new WMPLib.WindowsMediaPlayer();
+            Player.URL = url;
+            Player.controls.play();
+        }
+
+        private void Player_PlayStateChange(int NewState)
+        {
+            if ((WMPLib.WMPPlayState)NewState == WMPLib.WMPPlayState.wmppsStopped)
+            {
+                //Actions on stop
+            }
+        }
+    }
+}
